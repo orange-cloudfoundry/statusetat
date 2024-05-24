c95af1ad91113ba5f3f23bd562c14e253340cb67
Romain Dartigues <romain.dartigues.ext@orange.com>
2024-05-24 10:15:30 +0200

    bump github.com/onsi/ginkgo from 1.16.5 to v2.18.0

--- notifiers/slack/slack_suite_test.go
+++ notifiers/slack/slack_suite_test.go
@@ -3,7 +3,7 @@ package slack_test
 import (
 	"testing"
 
-	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
 
--- storages/db_test.go
+++ storages/db_test.go
@@ -5,7 +5,7 @@ import (
 	"time"
 
 	"github.com/jinzhu/gorm"
-	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
 	"github.com/orange-cloudfoundry/statusetat/models"
