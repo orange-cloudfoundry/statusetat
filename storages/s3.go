f9acb340942e4da44dba34774f286be106061162
Sara Mendaci <sara.mendaci@orange.com>
2025-08-26 14:06:25 +0200

    migrate AWS SDK Go v1 to v2

--- storages/s3.go
+++ storages/s3.go
@@ -2,6 +2,7 @@ package storages
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"net/url"
 	"os"
@@ -9,22 +10,21 @@ import (
 	"strings"
 	"time"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/awserr"
-	"github.com/aws/aws-sdk-go/aws/credentials"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3"
-	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/feature/s3/manager"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/orange-cloudfoundry/statusetat/v2/common"
 	"github.com/orange-cloudfoundry/statusetat/v2/models"
 	"github.com/orange-cloudfoundry/statusetat/v2/utils"
 )
 
 type s3Session struct {
-	bucket  string
-	path    string
-	svc     *s3.S3
-	awsSess *session.Session
+	bucket string
+	path   string
+	client *s3.Client
+	cfg    aws.Config
 }
 
 type S3 struct {
@@ -37,9 +37,9 @@ func (s *S3) Create(incident models.Incident) (models.Incident, error) {
 		return incident, err
 	}
 	b, _ := json.Marshal(incident)
-	uploader := s3manager.NewUploader(s.sess.awsSess)
-	_, err := uploader.Upload(&s3manager.UploadInput{
-		Bucket: &s.sess.bucket,
+	uploader := manager.NewUploader(s.sess.client)
+	_, err := uploader.Upload(context.TODO(), &s3.PutObjectInput{
+		Bucket: aws.String(s.sess.bucket),
 		Key:    aws.String(incident.GUID),
 		Body:   bytes.NewBuffer(b),
 	})
@@ -48,12 +48,12 @@ func (s *S3) Create(incident models.Incident) (models.Incident, error) {
 }
 
 func (s *S3) retrieveSubscribers() ([]string, error) {
-	obj, err := s.sess.svc.GetObject(&s3.GetObjectInput{
-		Bucket: &s.sess.bucket,
+	obj, err := s.sess.client.GetObject(context.TODO(), &s3.GetObjectInput{
+		Bucket: aws.String(s.sess.bucket),
 		Key:    aws.String(subscriberFilename),
 	})
 	if err != nil {
-		if aerr, ok := err.(awserr.RequestFailure); ok && aerr.StatusCode() == 404 {
+		if strings.Contains(err.Error(), "NoSuchKey") || strings.Contains(err.Error(), "404") {
 			return []string{}, os.ErrNotExist
 		}
 		return []string{}, err
@@ -69,9 +69,9 @@ func (s *S3) retrieveSubscribers() ([]string, error) {
 
 func (s *S3) storeSubscribers(subscribers []string) error {
 	b, _ := json.Marshal(subscribers)
-	uploader := s3manager.NewUploader(s.sess.awsSess)
-	_, err := uploader.Upload(&s3manager.UploadInput{
-		Bucket: &s.sess.bucket,
+	uploader := manager.NewUploader(s.sess.client)
+	_, err := uploader.Upload(context.TODO(), &s3.PutObjectInput{
+		Bucket: aws.String(s.sess.bucket),
 		Key:    aws.String(subscriberFilename),
 		Body:   bytes.NewBuffer(b),
 	})
@@ -126,9 +126,9 @@ func (s *S3) Unsubscribe(email string) error {
 func (s *S3) storePersistents(incidents []models.Incident) error {
 	sort.Sort(models.Incidents(incidents))
 	b, _ := json.Marshal(incidents)
-	uploader := s3manager.NewUploader(s.sess.awsSess)
-	_, err := uploader.Upload(&s3manager.UploadInput{
-		Bucket: &s.sess.bucket,
+	uploader := manager.NewUploader(s.sess.client)
+	_, err := uploader.Upload(context.TODO(), &s3.PutObjectInput{
+		Bucket: aws.String(s.sess.bucket),
 		Key:    aws.String(persistentFilename),
 		Body:   bytes.NewBuffer(b),
 	})
@@ -155,8 +155,8 @@ func (s *S3) Delete(guid string) error {
 	if err != nil {
 		return err
 	}
-	_, err = s.sess.svc.DeleteObject(&s3.DeleteObjectInput{
-		Bucket: &s.sess.bucket,
+	_, err = s.sess.client.DeleteObject(context.TODO(), &s3.DeleteObjectInput{
+		Bucket: aws.String(s.sess.bucket),
 		Key:    aws.String(guid),
 	})
 	return err
@@ -164,12 +164,12 @@ func (s *S3) Delete(guid string) error {
 
 func (s *S3) Read(guid string) (models.Incident, error) {
 	var incident models.Incident
-	obj, err := s.sess.svc.GetObject(&s3.GetObjectInput{
-		Bucket: &s.sess.bucket,
+	obj, err := s.sess.client.GetObject(context.TODO(), &s3.GetObjectInput{
+		Bucket: aws.String(s.sess.bucket),
 		Key:    aws.String(guid),
 	})
 	if err != nil {
-		if aerr, ok := err.(awserr.RequestFailure); ok && aerr.StatusCode() == 404 {
+		if strings.Contains(err.Error(), "NoSuchKey") || strings.Contains(err.Error(), "404") {
 			incident, err := s.readPersistent(guid)
 			if err != nil {
 				return models.Incident{}, os.ErrNotExist
@@ -191,8 +191,8 @@ func (s *S3) Read(guid string) (models.Incident, error) {
 }
 
 func (s *S3) ByDate(from, to time.Time) ([]models.Incident, error) {
-	objs, err := s.sess.svc.ListObjects(&s3.ListObjectsInput{
-		Bucket: &s.sess.bucket,
+	objs, err := s.sess.client.ListObjects(context.TODO(), &s3.ListObjectsInput{
+		Bucket: aws.String(s.sess.bucket),
 	})
 	if err != nil {
 		return []models.Incident{}, err
@@ -218,8 +218,8 @@ func (s *S3) ByDate(from, to time.Time) ([]models.Incident, error) {
 }
 
 func (s *S3) Ping() error {
-	_, err := s.sess.svc.HeadBucket(&s3.HeadBucketInput{
-		Bucket: &s.sess.bucket,
+	_, err := s.sess.client.HeadBucket(context.TODO(), &s3.HeadBucketInput{
+		Bucket: aws.String(s.sess.bucket),
 	})
 	return err
 }
@@ -241,61 +241,84 @@ func (s *S3) Detect(u *url.URL) bool {
 }
 
 func (s *S3) urlToSession(u *url.URL) (*s3Session, error) {
+	bucket, path := s.extractBucketPath(u)
+	
+	// Use default config if connecting to standard AWS S3
 	if strings.HasSuffix(u.Host, "s3.amazonaws.com") && (u.User == nil || u.User.Username() == "") {
-		bucket, path := s.extractBucketPath(u)
-		sess, err := session.NewSession()
+		cfg, err := config.LoadDefaultConfig(context.TODO())
 		if err != nil {
 			return nil, err
 		}
 		return &s3Session{
-			bucket:  bucket,
-			path:    path,
-			svc:     s3.New(sess),
-			awsSess: sess,
+			bucket: bucket,
+			path:   path,
+			client: s3.NewFromConfig(cfg),
+			cfg:    cfg,
 		}, nil
 	}
+
+	// Handle custom S3-compatible endpoints
 	pathStyle := true
-	bucket, path := s.extractBucketPath(u)
 	host := u.Host
 	if strings.HasSuffix(host, ".s3.amazonaws.com") {
 		host = strings.TrimPrefix(host, bucket+".")
 		pathStyle = false
 	}
-	creds := credentials.NewEnvCredentials()
-	if u.User != nil || u.User.Username() != "" {
+
+	var credProvider aws.CredentialsProvider
+	if u.User != nil && u.User.Username() != "" {
 		pass, _ := u.User.Password()
-		creds = credentials.NewStaticCredentials(u.User.Username(), pass, "")
+		credProvider = credentials.NewStaticCredentialsProvider(u.User.Username(), pass, "")
+	} else {
+		// Use default credentials chain (environment variables, IAM role, etc.)
+		credProvider = nil
 	}
+
 	region := "us-east-1"
 	if u.Query().Get("region") != "" {
 		region = u.Query().Get("region")
 	}
-	u.User = nil
-	sess, err := session.NewSession(&aws.Config{
-		Credentials:      creds,
-		Endpoint:         &host,
-		Region:           &region,
-		HTTPClient:       makeHttpClient(u),
-		S3ForcePathStyle: &pathStyle,
-	})
+
+	var cfg aws.Config
+	var err error
+	if credProvider != nil {
+		cfg, err = config.LoadDefaultConfig(context.TODO(),
+			config.WithCredentialsProvider(credProvider),
+			config.WithRegion(region),
+			config.WithHTTPClient(makeHttpClient(u)),
+		)
+	} else {
+		cfg, err = config.LoadDefaultConfig(context.TODO(),
+			config.WithRegion(region),
+			config.WithHTTPClient(makeHttpClient(u)),
+		)
+	}
 	if err != nil {
 		return nil, err
 	}
+
+	client := s3.NewFromConfig(cfg, func(o *s3.Options) {
+		o.UsePathStyle = pathStyle
+		if host != u.Host {
+			o.BaseEndpoint = aws.String("https://" + host)
+		}
+	})
+
 	return &s3Session{
-		bucket:  bucket,
-		path:    path,
-		svc:     s3.New(sess),
-		awsSess: sess,
+		bucket: bucket,
+		path:   path,
+		client: client,
+		cfg:    cfg,
 	}, nil
 }
 
 func (s *S3) Persistents() ([]models.Incident, error) {
-	obj, err := s.sess.svc.GetObject(&s3.GetObjectInput{
-		Bucket: &s.sess.bucket,
+	obj, err := s.sess.client.GetObject(context.TODO(), &s3.GetObjectInput{
+		Bucket: aws.String(s.sess.bucket),
 		Key:    aws.String(persistentFilename),
 	})
 	if err != nil {
-		if aerr, ok := err.(awserr.RequestFailure); ok && aerr.StatusCode() == 404 {
+		if strings.Contains(err.Error(), "NoSuchKey") || strings.Contains(err.Error(), "404") {
 			return []models.Incident{}, nil
 		}
 		return []models.Incident{}, err
